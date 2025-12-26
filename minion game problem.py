c274621c34acf33fbac36b90f1f39e73964c4104
leeladhar-7star <leeladhar.alukunta@gmail.com>
2025-12-26 19:38:48 +0530

    Add files via upload

---
+++ minion game problem.py
@@ -0,0 +1,16 @@
+def minion_game(string):
+# your code goes here
+    k=0
+    s=0
+    v="AEIOU"
+    for i in range (len(string)):
+        if string[i] in v:
+            k+=len(string)-i
+        else:
+            s+=len(string)-i
+        if k>s:
+            print("Kevin",k)
+        elif s>k:
+            print("Stuart",s)
+        else:
+            print("Draw")
