4ee52bbc9accd8e29f55c20b57ffef3694ea2d81
leeladhar-7star <leeladhar.alukunta@gmail.com>
2025-12-29 20:17:46 +0530

    Add files via upload

---
+++ student class 3 subjects.py
@@ -0,0 +1,11 @@
+ def __init_(self,name,m1,m2,m3):
+        self.name=name
+        self.m1=m1
+        self.m2=m2
+        self.m3=m3
+    def average(self):
+        avg =(self.m1+self.m2+self.m3)/m3 
+        print("Name:",self.name)
+        print("average marks:",avg)
+      s1=student("leela",60,70,80)
+      s1.average()
